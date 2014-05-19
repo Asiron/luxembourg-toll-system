7362102f01555cb975dc7fbb65607dc9888d3edd
Maciej <maciej.zurad@gmail.com>
2014-05-19 15:49:56 +0200

    Adding unit test directory

---
+++ src/uni/lu/lts/TollFacility.java
@@ -0,0 +1,13 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package uni.lu.lts;
+
+/**
+ *
+ * @author asiron
+ */
+class TollFacility {
+    
+}
---
+++ src/uni/lu/lts/Truck.java
@@ -0,0 +1,9 @@
+package uni.lu.lts;
+
+public class Truck extends Vehicle {
+
+    public Truck(VehicleType vehicleType, String numberPlate, String country, float size) {
+        super(vehicleType, numberPlate, country, size);
+    }
+       
+}
